8324f8a4842d0c9c95ac98c311fffbf190c987f0
SMILEY <kariukigeorge2030@gmail.com>
2021-08-01 21:48:06 +0300

    project structure

---
+++ README.md
@@ -0,0 +1,9 @@
+## Uber Eats Backend
+
+I'm working on creating a restful api with mongodb database
+
+## Technologies used
+
+- node js
+- express js framework
+- mongodb
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  return res.status(200).json("Hello you");
+  return res.status(200).json("Hello my friend");
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
